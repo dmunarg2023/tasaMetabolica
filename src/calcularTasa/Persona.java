dc94550acecb3050dad6c41454fe6444a980f22f
dmunarg2023 <142017323+dmunarg2023@users.noreply.github.com>
2024-02-21 09:31:08 -0500

    final

--- src/calcularTasa/Persona.java
+++ src/calcularTasa/Persona.java
@@ -3,58 +3,49 @@ public class Persona{
 	private int peso;
 	private int altura;
 	private int edad;	
+
 	
+	 public int calcularTasaMetabolica() {
+	        return 0; 
+	    }
+	class Hombre extends Persona {
+
+	    public int calcularTasaMetabolica() {
+	        return (int) (88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * edad));
+	    }
+	}
+
+	class Mujer extends Persona {
+
+	    public int calcularTasaMetabolica() {
+	        return (int) (447.593 + (9.247 * peso) + (3.098 * altura) - (4.33 * edad));
+	    }
+	    
+	    public int getPeso() {
+	        return peso;
+	    }
 	
+	    public void setPeso(int peso) {
+	        this.peso = peso;
+	    }
 	
-	public class hombre extends Persona{
-		private int peso;
-		private int altura;
-		private int edad;	
-		
-		int TMB = (int) (88.362 + (13.397 *peso ) + (4.799  *altura ) - (5.677 * edad));
-	}
-
-	public class mujer extends Persona{
-		
-		private int peso;
-		private int altura;
-		private int edad;	
-		
-		int TMB = (int) (447.593 + (9.247 *peso ) + (3.098 *altura ) - (4.33  * edad));
-		
-	}
-
-	public void calcularTasaMetabolica(){
-		
-	}
+	    public int getAltura() {
+	        return altura;
+	    }
 	
+	    public void setAltura(int altura) {
+	        this.altura = altura;
+	    }
 	
+	    public int getEdad() {
+	        return edad;
+	    }
 	
-	public int getPeso() {
-		return this.peso;
+	    public void setEdad(int edad) {
+	        this.edad = edad;
+	    }
+
+
 	}
 
-
-
-	public int getAltura() {
-		return altura;
-	}
-
-
-
-	public void setAltura(int altura) {
-		this.altura = altura;
-	}
-
-
-
-	public int getEdad() {
-		return edad;
-	}
-
-
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
 }
